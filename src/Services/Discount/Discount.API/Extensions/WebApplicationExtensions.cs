d0d7b2a4787ed37cd65ff1cdda3aa750860963f7
abhijeet <abhijeetpatil98987@gmail.com>
2024-02-12 23:46:50 +0530

    Docker and discount service added with migrations for seed data

---
+++ src/Services/Discount/Discount.API/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Npgsql;
+
+namespace Discount.API.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static WebApplication MigrateDatabse<TContext>(this WebApplication app, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating postresql database.");
+                    using var connection = new NpgsqlConnection(
+                        configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection,
+                    };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                                ProductName TEXT NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES ('IPhone X','IPhone Discount',150);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES ('Samsung 10','Samsung Discount',100);";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Migrated postresql database.");
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occured while migrating the postresql database");
+
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        Thread.Sleep(2000);
+                        MigrateDatabse<TContext>(app, retryForAvailability);
+                    }
+                }
+
+            }
+            return app;
+        }
+    }
+}
